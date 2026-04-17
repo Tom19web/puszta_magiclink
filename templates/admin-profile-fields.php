902ac3b30239dee36bfffbeed719b958cab68f1e
Tom19web <tamasszili6@gmail.com>
2026-04-17 11:14:12 +0200

    feat: Xtream jelszó mező hozzáadva az admin profilhoz

--- templates/admin-profile-fields.php
+++ templates/admin-profile-fields.php
@@ -3,10 +3,25 @@
 <p>Ahol az isteni hatalmad manifesztálódik a profiljukon.</p>
 <table class="form-table">
     <tr>
-        <th><label for="pp_client_id">Ügyfélkód</label></th>
+        <th><label for="pp_client_id">Ügyfélkód (Xtream username)</label></th>
         <td>
             <input type="text" name="pp_client_id" id="pp_client_id" value="<?php echo esc_attr($client_id); ?>" class="regular-text" />
-            <p class="description">A rabszolga azonosítója.</p>
+            <p class="description">A rabszolga azonosítója. Egyben az Xtream Codes felhasználónév.</p>
+        </td>
+    </tr>
+    <tr>
+        <th><label for="pp_xtream_pass">Xtream jelszó</label></th>
+        <td>
+            <input type="password" name="pp_xtream_pass" id="pp_xtream_pass"
+                value="<?php echo esc_attr(pp_get_xtream_pass($user->ID)); ?>"
+                class="regular-text"
+                autocomplete="new-password" />
+            <p class="description">
+                Csak admin látja. A lejátszó plugin automatikusan ezzel lépteti be a felhasználót.
+                <?php if (pp_get_xtream_pass($user->ID)) : ?>
+                    <br><strong style="color:#2ea44f">✓ Jelszó mentve</strong>
+                <?php endif; ?>
+            </p>
         </td>
     </tr>
     <tr>
